632632219967ed00591f8aaef7c0a2f0da4dfd14
codex2019 <codyx2019@gmail.com>
2021-05-10 20:06:35 -0700

    first commit

---
+++ JobSearch/Controller/SearchViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  JobSearch
+//
+//  Created by MacX on 3/31/21.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchTextField.delegate = self
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let title = searchTextField.text {
+            let resultVC = ResultsViewController()
+            resultVC.jobTitle = title
+        }
+        searchTextField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "please enter job title"
+            return false
+        }
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+}
+
